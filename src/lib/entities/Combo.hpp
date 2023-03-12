58471101892a5ab50c17a9187b9f6bd8526eead8
Bitha17 <16521076@mahasiswa.itb.ac.id>
2023-03-12 15:41:06 +0700

    feat: Combo.hpp temp

--- src/lib/entities/Combo.hpp
+++ src/lib/entities/Combo.hpp
@@ -24,12 +24,87 @@ class Combo : public Valuable
     static vector<Combo*>& getCombos();
 
   protected:
-    std::vector<Card> cards;
+    vector<Card> cards;
 
   private:
     const string name;
 
     static vector<Combo*> combos;
+};
+
+// The following classes inherit from the Combo class and implement the isThereCombo()
+// and clone() functions in their own specific way
+
+class HighCard : public Combo
+{
+  public:
+    HighCard(Card c);
+    bool isThereCombo(vector<Card>& cards) override;
+    Combo* clone() override;
+};
+
+class Pair: public Combo
+{
+  public:
+    Pair(vector<Card> _cards);
+    bool isThereCombo(vector<Card>& cards) override;
+    Combo* clone() override;
+};
+
+class TwoPair : public Combo
+{
+  public:
+    TwoPair(vector<Card> _cards);
+    bool isThereCombo(vector<Card>& cards) override;
+    Combo* clone() override;
+};
+
+class ThreeOfAKind : public Combo
+{
+  public:
+    ThreeOfAKind(vector<Card> _cards);
+    bool isThereCombo(vector<Card>& cards) override;
+    Combo* clone() override;
+};
+
+class Straight : public Combo
+{
+  public:
+    Straight(vector<Card> _cards);
+    bool isThereCombo(vector<Card>& cards) override;
+    Combo* clone() override;
+};
+
+class Flush : public Combo
+{
+  public:
+    Flush(vector<Card> _cards);
+    bool isThereCombo(vector<Card>& cards) override;
+    Combo* clone() override;
+};
+
+class FullHouse : public Combo
+{
+  public:
+    FullHouse(vector<Card> _cards);
+    bool isThereCombo(vector<Card>& cards) override;
+    Combo* clone() override;
+};
+
+class FourOfAKind : public Combo
+{
+  public:
+    FourOfAKind(vector<Card> _cards);
+    bool isThereCombo(vector<Card>& cards) override;
+    Combo* clone() override;
+};
+
+class StraightFlush : public Combo
+{
+  public:
+    StraightFlush(vector<Card> _cards);
+    bool isThereCombo(vector<Card>& cards) override;
+    Combo* clone() override;
 };
 
 #endif // !__COMBO_
