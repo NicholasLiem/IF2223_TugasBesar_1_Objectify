36fd2c4d66e599d82630206b1b15bd6bba6ffb5f
msfir <ms.firdaus777@gmail.com>
2023-03-15 12:51:28 +0700

    Add capability to read configuraion file

--- src/apps/entities/Card.cpp
+++ src/apps/entities/Card.cpp
@@ -1,6 +1,9 @@
-#include "../../lib/entities/Card.hpp"
+#include "Card.hpp"
+
+#include "Utils.hpp"
 
 #include <iostream>
+#include <string>
 using namespace std;
 
 Card::Card(const CardColor color, const CardNumber number)
@@ -36,7 +39,8 @@ CardNumber Card::getNumber() const
     return number;
 }
 
-bool Card::operator==(const Card& card1){
+bool Card::operator==(const Card& card1)
+{
     return (this->getNumber() == card1.getNumber());
 }
 
@@ -62,24 +66,78 @@ bool operator>(const Card& card1, const Card& card2)
     return (card1.value() > card2.value());
 }
 
-ostream& operator<<(ostream& os, const Card& card)
+ostream& operator<<(ostream& out, const CardColor& color)
 {
-    CardNumber card_num = card.getNumber();
-    CardColor card_color = card.getColor();
-    os << int(card_num) << " ";
-    switch (card_color) {
+    switch (color) {
     case CardColor::Red:
-        os << "Merah";
+        out << "Merah";
         break;
     case CardColor::Green:
-        os << "Hijau";
+        out << "Hijau";
         break;
     case CardColor::Blue:
-        os << "Biru";
+        out << "Biru";
         break;
     case CardColor::Yellow:
-        os << "Kuning";
+        out << "Kuning";
         break;
     }
+    return out;
+}
+
+ostream& operator<<(ostream& out, const CardNumber& number)
+{
+    out << int(number);
+    return out;
+}
+
+ostream& operator<<(ostream& os, const Card& card)
+{
+    os << card.getNumber() << " " << card.getColor();
     return os;
+}
+
+istream& operator>>(istream& in, CardColor& color)
+{
+    string line;
+    in >> line;
+    string str_color = Utils::to_lower(line);
+    if (str_color == "merah") {
+        color = CardColor::Red;
+    } else if (str_color == "kuning") {
+        color = CardColor::Yellow;
+    } else if (str_color == "biru") {
+        color = CardColor::Blue;
+    } else if (str_color == "hijau") {
+        color = CardColor::Green;
+    } else {
+        throw "Invalid card color: " + line;
+    }
+    return in;
+}
+
+istream& operator>>(istream& in, CardNumber& number)
+{
+    int num;
+    in >> num;
+    if (num < 1 || num > 13) {
+        throw "Invalid card number: " + to_string(num);
+    } else {
+        number = static_cast<CardNumber>(num);
+    }
+    return in;
+}
+
+istream& operator>>(istream& in, Card& card)
+{
+    CardNumber number;
+    CardColor color;
+    try {
+        in >> number >> color;
+        card.number = number;
+        card.color = color;
+    } catch (const string& e) {
+        throw "Error parsing string: " + string(e);
+    }
+    return in;
 }
